81ce9243a4bb276e14900286cf70c86b8bf1ef9b
Hugo <rytlewski.hugo@etu.educ-valadon-limoges.fr>
2023-07-25 17:00:55 +0200

    ggg

--- components/Layouts/Footer.vue
+++ components/Layouts/Footer.vue
@@ -2,7 +2,7 @@
     <footer class="bg-neutral-900 rounded-lg m-4 ">
         <div class="w-screen-xl items-center p-4 flex flex-col md:flex-row md:justify-between">
             <span class="text-md text-white sm:text-center ">© 2023 
-                <a class="hover:underline">Portfolio Hugo, Tous droits réservés</a>
+                <a class="">Portfolio Hugo, Tous droits réservés</a>
         
             </span>
             <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-white mr-0 gap-4 md:mr-16 sm:mt-0">
@@ -16,11 +16,7 @@
                         <img  class="w-6" src="~/assets/img/githubwhite.png" alt="">    
                     </a>
                 </li>
-                <li>
-                    <a href="https://www.instagram.com/hugo.rytl/">
-                        <img  class="w-6" src="~/assets/img/instawhite.png" alt="">    
-                    </a>
-                </li>
+                
             </ul>
         </div>
     </footer>
